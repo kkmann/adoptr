b72cfea18c67a68e3e9968efa93efc8a22a0da0c
Kevin <kevinkunzmann@gmx.net>
2018-11-30 13:20:41 +0000

    reorganizing Design implementation

--- R/Score.R
+++ R/Score.R
@@ -1,9 +1,9 @@
 setClass("ConditionalScore", representation(prior = "Distribution"))
 
-# to be defined by each score, eval(s, design, z1, ...) for conditional scores
-setGeneric("eval",
-           function(s, design, ...) standardGeneric("eval")
-)
+# evaluate a score
+setGeneric("eval", function(s, design, ...) standardGeneric("eval"))
+# allow cusotm implementation depending on design
+setGeneric(".eval_specific", function(s, design, ...) standardGeneric(".eval_specific"))
 
 # better name? integrate a conditional score with respect to Z1
 setGeneric("integrate",
@@ -25,12 +25,25 @@ setClass("IntegralScore", representation(conditional_score = "ConditionalScore")
 # generic evaluation of integral score, this is where the problems lie ;)
 # uses stats::integrate to integrate conditional score over Z1, not working for
 # optimization - need custom implementation for signature("IntegralScore", "BSDesign")
-setMethod("eval",
-          signature("IntegralScore", "Design"),
-          function(s, design, ...) {
-                  integrand <- function(z1) eval(s@conditional_score, design, z1, ...) *
-                          predictive_pdf(s@conditional_score@prior, z1, n1(design), ...)
-                  z1_bounds <- get_z1_bounds(s@conditional_score@prior, mass = .99998)
-                  return(stats::integrate(integrand, z1_bounds[1], z1_bounds[2])$value)
-          }
-)
+setMethod("eval", signature("IntegralScore", "Design"),
+    function(s, design, specific = TRUE, ...) {
+        # TODO: currently ignores the possibility of early stopping/uncontinuus
+        # conditional scores - might get better when checking for early stopping
+        # and integrating separately!
+        if (specific) { # use design-specific implementation
+            return(.eval_specific(s, design, ...))
+        } else { # use generic approach
+            # integrand is the conditional score as function of z1 times the
+            # predictive pdf given the scores prior
+            integrand <- function(z1) eval(s@conditional_score, design, z1, ...) *
+                predictive_pdf(s@conditional_score@prior, z1, n1(design), ...)
+            z1_bounds <- get_z1_bounds(s@conditional_score@prior, mass = .99998)
+            # use adaptive quadrature to integrate - only relies on generic interface
+            # provided by 'Design', no special optimization for particular
+            # design implementation
+            return(stats::integrate(integrand, z1_bounds[1], z1_bounds[2])$value)
+        }
+    })
+
+setMethod(".eval_specific", signature("IntegralScore", "Design"),
+    function(s, design, ...) stop("not implemented") )
