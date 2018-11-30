8a0d8e51fcc55114ff3a0b4b6bdabd754f9730ae
Kevin <kevinkunzmann@gmx.net>
2018-11-30 15:53:14 +0000

    refactoring classes

--- R/IntegralScore.R
+++ R/IntegralScore.R
@@ -1,25 +1,3 @@
-setClass("ConditionalScore", representation(prior = "Prior"))
-
-# evaluate a score
-setGeneric("eval", function(s, design, ...) standardGeneric("eval"))
-# allow cusotm implementation depending on design
-setGeneric(".eval_specific", function(s, design, ...) standardGeneric(".eval_specific"))
-
-# better name? integrate a conditional score with respect to Z1
-setGeneric("integrate",
-           function(s, ...) standardGeneric("integrate")
-)
-
-# essentialy just changes class and stores conditional score to allow
-# different implementation of 'eval'
-setMethod("integrate",
-          signature("ConditionalScore"),
-          function(s, ...) {
-                  new("IntegralScore", conditional_score = s)
-          }
-)
-
-# well, result of 'integrate'
 setClass("IntegralScore", representation(conditional_score = "ConditionalScore"))
 
 # generic evaluation of integral score, this is where the problems lie ;)
