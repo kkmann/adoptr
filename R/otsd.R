79c575119fd05563e3b4199ef7719ca8ba7cb951
Kevin <kevinkunzmann@gmx.net>
2018-11-30 18:46:09 +0000

    fiddling with vignettes

---
+++ R/otsd.R
@@ -0,0 +1,26 @@
+#' Optimal Two-Stage Designs
+#'
+#' This package provides functionality to compute and analyze optimal
+#' two-stage designs for two-stage superiority tests based on a Z statistic.
+#' A range of pre-implemented objective/constraint functions are provided.
+#'
+#' @section Quickstart:
+#'
+#' For a sample workflow and a quick demo of the capabilities, see the
+#' vignette [todo: link].
+#'
+#' @section Designs:
+#'
+#' list designs
+#'
+#' @section Prios:
+#'
+#' List prior classes
+#'
+#' @section Scores:
+#'
+#' list score classes
+#'
+#' @docType package
+#' @name otsd
+NULL
