14d608ad96c3b0381428e901ff6a8c31b3f95697
Kevin <kevinkunzmann@gmx.net>
2019-02-04 23:14:53 +0000

    increasing run time of optimizer + precision solves the problem without regularization!

--- tests/testthat/test_examples.R
+++ tests/testthat/test_examples.R
@@ -55,4 +55,41 @@ test_that("plausible n2() function", {
 
     expect_equal(1, 0) # just indicate that we have a problem ;)
 
+    evaluate(ess, optimal_design)
+
+    # manally fix the problem
+    optimal_design@n2_pivots[order] <- 18
+
+    evaluate(ess, optimal_design) # better result -> expected sample size could be ok
+
+    # what about increasing runtime?
+    optimal_design <- minimize(
+        ess,
+        subject_to(
+            pow  >= 0.8,
+            toer <= .05
+        ),
+        initial_design = optimal_design,
+        lower_boundary_design = update(design, c(10, -1, 1, numeric(order) + 2, numeric(order) - 5)),
+        upper_boundary_design = update(design, c(50, 1, 4, numeric(order) + 50, numeric(order) + 5)),
+        opts = list(
+            algorithm   = "NLOPT_LN_COBYLA",
+            xtol_rel    = 1e-5,
+            maxeval     = 10000, # increased precision
+            print_level = 1
+        )
+    )
+
+    plot(optimal_design)
+
+    z1 <- seq(0, 2.5, by = .01)
+    plot(
+        z1,
+        n(optimal_design, z1), ylim = c(0, 1.1*(optimal_design@n1 + max(optimal_design@n2_pivots))),
+        'l'
+    )
+
+    # much better!
+
+
 })
