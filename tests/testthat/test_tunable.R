4af844a6d8ae89b9444cb715db8383effdd49b56
Kevin <kevinkunzmann@gmx.net>
2019-02-20 11:43:26 +0000

    revised test_tunable; only testing absolute minumum; added a few todos for missing cases!

--- tests/testthat/test_tunable.R
+++ tests/testthat/test_tunable.R
@@ -1,67 +1,81 @@
 context("tunable parameters")
 
-
-
+# preliminaries
 n1     <- 25
 c1f    <-   .0
 c1e    <-  2.0
 order  <- 5L
 n2_piv <- rep(40.0, order)
 c2_piv <- rep(1.96, order)
-design <- gq_design(n1, c1f, c1e, n2_piv, c2_piv, order)
 
-# check if key figures can be computed
+initial_design <- gq_design(n1, c1f, c1e, n2_piv, c2_piv, order)
+
+dist        <- Normal(two_armed = FALSE)
 null        <- PointMassPrior(.0, 1)
 alternative <- PointMassPrior(.4, 1)
 
-dist <- Normal(two_armed = FALSE)
-
 ess  <- integrate(ConditionalSampleSize(dist, alternative))
-cp   <- ConditionalPower(dist, alternative)
-pow  <- integrate(cp)
+pow  <- integrate(ConditionalPower(dist, alternative))
 toer <- integrate(ConditionalPower(dist, null))
-smth <- AverageN2()
+
+
 
 
 
 test_that("tunability of parameters can be changed", {
 
-    tmp <- make_fixed(design, n1, c1f, c1e)
+    tmp <- make_fixed(initial_design, n1, c1f, c1e)
 
     expect_true(!tmp@tunable["n1"])
     expect_true(!tmp@tunable["c1f"])
     expect_true(!tmp@tunable["c1e"])
-    expect_equal(tmp@tunable["n2_pivots"], design@tunable["n2_pivots"])
-    expect_equal(tmp@tunable["c2_pivots"], design@tunable["c2_pivots"])
-    expect_equal(tmp@tunable["x1_norm_pivots"], design@tunable["x1_norm_pivots"])
-    expect_equal(tmp@tunable["weights"], design@tunable["weights"])
-    expect_equal(tmp@tunable["tunable"], design@tunable["tunable"])
+    expect_equal(tmp@tunable["n2_pivots"], initial_design@tunable["n2_pivots"])
+    expect_equal(tmp@tunable["c2_pivots"], initial_design@tunable["c2_pivots"])
+    expect_equal(tmp@tunable["x1_norm_pivots"], initial_design@tunable["x1_norm_pivots"])
+    expect_equal(tmp@tunable["weights"], initial_design@tunable["weights"])
+    expect_equal(tmp@tunable["tunable"], initial_design@tunable["tunable"])
 
 })
+
+# TODO: does make_tunable work as well?
 
 
 
 test_that("two stage design can be optimized with fixed first stage", {
 
-    tmp <- make_fixed(design, n1, c1f, c1e)
+    tmp <- make_fixed(initial_design, n1, c1f, c1e)
 
+    lb_design      <- update(tmp, c(numeric(order) + 1, numeric(order) - 3))
+    ub_design      <- update(tmp, c(numeric(order) + 100, numeric(order) + 10))
 
-    suppressWarnings(
-    opt_design <- minimize(
-        ess + 0.0001 * smth,
+    res <- minimize(
+
+        ess,
         subject_to(
             pow  >= 0.8,
             toer <= .05
         ),
+
+        post_process          = FALSE, # if we can do it without post
+                                       # processing, by design, it also
+                                       # works with
         initial_design        = tmp,
-        lower_boundary_design = update(tmp, c(numeric(order) + 2, numeric(order) - 5)),
-        upper_boundary_design = update(tmp, c(numeric(order) + 50, numeric(order) + 5))
-    )$design
+        lower_boundary_design = lb_design,
+        upper_boundary_design = ub_design,
+        opts = list(
+            algorithm   = "NLOPT_LN_COBYLA",
+            xtol_abs    = 1 # we do not need convergence,
+                            # only see if it works technically!
+        )
     )
 
-    expect_equal(opt_design@n1, tmp@n1)
-    expect_equal(opt_design@c1f, tmp@c1f)
-    expect_equal(opt_design@c1e, tmp@c1e)
+    # make sure that the lax convergence still leads to function evaluations!
+    expect_true(res$details$nloptr_return$iterations >= 10)
+
+    # check that fixed params did not change
+    expect_equal(res$design@n1, tmp@n1)
+    expect_equal(res$design@c1f, tmp@c1f)
+    expect_equal(res$design@c1e, tmp@c1e)
 
 })
 
@@ -69,43 +83,39 @@ test_that("two stage design can be optimized with fixed first stage", {
 
 test_that("two stage design can be optimized with fixed sample sizes", {
 
-    # minimize design over all parameters
-    suppressWarnings(
-    opt_design <- minimize(
-        ess + 0.0001*smth,
+    tmp <- make_fixed(initial_design, n1, n2_pivots)
+
+    lb_design      <- update(tmp, c(-1, 1, numeric(order) - 3))
+    ub_design      <- update(tmp, c(1, 4, numeric(order) + 5))
+
+    res <- minimize(
+
+        ess,
         subject_to(
             pow  >= 0.8,
             toer <= .05
         ),
-        initial_design = design,
-        lower_boundary_design = update(design, c(10, -1, 1, numeric(order) + 2, numeric(order) - 5)),
-        upper_boundary_design = update(design, c(50, 1, 4, numeric(order) + 50, numeric(order) + 5))
-    )$design
-    )
 
-    # round sample sizes to nearest integers
-    # TODO: we need an evalution mode where n2 is also rounded to the next integer!
-    opt_design@n1        <- round(opt_design@n1)
-    opt_design@n2_pivots <- round(opt_design@n2_pivots)
-
-    tmp <- make_fixed(opt_design, n1, n2_pivots)
-
-    # suppress initial design infeasible
-    suppressWarnings(
-    tmp <- minimize(
-        ess + 0.0001 * smth,
-        subject_to(
-            pow  >= 0.8,
-            toer <= .05
-        ),
+        post_process          = FALSE, # if we can do it without post
+                                       # processing, by design, it also
+                                       # works with
         initial_design        = tmp,
-        lower_boundary_design = update(tmp, c(-1, 1, numeric(order) - 5)),
-        upper_boundary_design = update(tmp, c(1, 4, numeric(order) + 5))
-    )$design
+        lower_boundary_design = lb_design,
+        upper_boundary_design = ub_design,
+        opts = list(
+            algorithm   = "NLOPT_LN_COBYLA",
+            xtol_abs    = 1 # we do not need convergence,
+                            # only see if it works technically!
+        )
+
     )
 
-    expect_equal(opt_design@n1, tmp@n1)
-    expect_equal(opt_design@n2_pivots, tmp@n2_pivots)
+    # make sure that the lax convergence still leads to function evaluations!
+    expect_true(res$details$nloptr_return$iterations >= 10)
+
+    # check that fixed params did not change
+    expect_equal(res$design@n1, tmp@n1)
+    expect_equal(res$design@n2_pivots, tmp@n2_pivots)
 
 })
 
