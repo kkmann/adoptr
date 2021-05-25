ac93b4c1500a9a0feb776d20b4823eb09910502b
Kevin Kunzmann <kevinkunzmann@gmx.net>
2021-05-25 21:50:01 +0100

    ...

--- tests/testthat/test_constraints.R
+++ tests/testthat/test_constraints.R
@@ -145,6 +145,12 @@ test_that("show method", {
 
     cp <- ConditionalPower(Normal(two_armed = FALSE), PointMassPrior(.4, 1))
 
+    tmp <- capture.output(show(cp >= 0.8))
+
+    print("\n\r")
+    print(tmp)
+    print("\n\r")
+
     expect_equal(
         capture.output(show(cp >= 0.8)),
         "-Pr[x2>=c2(x1)|x1] (x1) <= -0.8 for x1 in [c1f,c1e] "
